0b3682883ed5697a1da9b07c72e9b3df5a266001
Simonsator <00pflaume@gmail.com>
2022-08-04 15:03:25 +0200

    Use more modern apis

--- src/main/java/de/simonsator/partyandfriends/extentions/teleporttofriends/bungee/TTFBungeePlugin.java
+++ src/main/java/de/simonsator/partyandfriends/extentions/teleporttofriends/bungee/TTFBungeePlugin.java
@@ -1,10 +1,11 @@
 package de.simonsator.partyandfriends.extentions.teleporttofriends.bungee;
 
 import de.simonsator.partyandfriends.api.PAFExtension;
+import de.simonsator.partyandfriends.api.adapter.BukkitBungeeAdapter;
 import de.simonsator.partyandfriends.friends.commands.Friends;
 import de.simonsator.partyandfriends.friends.subcommands.Jump;
+import de.simonsator.partyandfriends.utilities.ConfigurationCreator;
 import net.md_5.bungee.api.ProxyServer;
-import net.md_5.bungee.config.Configuration;
 
 import java.io.File;
 import java.io.IOException;
@@ -14,9 +15,9 @@ public class TTFBungeePlugin extends PAFExtension {
 	@Override
 	public void onEnable() {
 		try {
-			Configuration config = new TTFBungeeConfig(new File(getConfigFolder(), "config.yml"), this).getCreatedConfiguration();
-			ProxyServer.getInstance().getPluginManager().registerListener(this,
-					new SendTeleportTask(config.getLong("TeleportDelay"), this, config.getString("Permission")));
+			ConfigurationCreator config = new TTFBungeeConfig(new File(getConfigFolder(), "config.yml"), this);
+			BukkitBungeeAdapter.getInstance().registerListener(new SendTeleportTask(config.getLong("TeleportDelay"), this, config.getString("Permission")),
+					this);
 			for (String server : config.getStringList("TeleportToPlayerServers")) {
 				((Jump) Friends.getInstance().getSubCommand(Jump.class)).doNotCheckForSameServer(ProxyServer.getInstance().getServerInfo(server));
 			}

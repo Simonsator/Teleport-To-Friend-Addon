9174aeadee77ef5c4ba32a38bcdfb68a17d12f26
J.T. McQuigg <J.T.McQuigg12@gmail.com>
2022-08-20 16:56:04 -0400

    Add Depend

--- src/main/java/de/simonsator/partyandfriends/extentions/teleporttofriends/velocity/TTFVelocityLoader.java
+++ src/main/java/de/simonsator/partyandfriends/extentions/teleporttofriends/velocity/TTFVelocityLoader.java
@@ -3,16 +3,17 @@ package de.simonsator.partyandfriends.extentions.teleporttofriends.velocity;
 import com.google.inject.Inject;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.plugin.Dependency;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import com.velocitypowered.api.proxy.ProxyServer;
-import de.simonsator.partyandfriends.velocity.VelocityExtensionLoadingInfo;
-import de.simonsator.partyandfriends.velocity.main.PAFPlugin;
+
 
 import java.nio.file.Path;
 
 @Plugin(id = "teleport-to-friend-paf-extension", name = "TeleportToFriendsAddon", version = "1.0.2-RELEASE",
-		url = "https://www.spigotmc.org/resources/51060/", description = "Loads Teleport To Friend PAF Extension", authors = {"JT122406", "Simonsator"})
+		url = "https://www.spigotmc.org/resources/51060/", description = "Loads Teleport To Friend PAF Extension", authors = {"JT122406", "Simonsator"}, dependencies = {@Dependency(id = "partyandfriends")})
+
 public class TTFVelocityLoader {
 	private final Path folder;
 	public static ProxyServer server = null;
